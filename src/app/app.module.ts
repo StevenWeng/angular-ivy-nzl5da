f02aa9631377e57db66ed563e11ec16be86bad9a
StevenWeng <wosteven@gmail.com>
2020-12-21 23:14:58 +0800

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { PagesComponent } from "./pages/pages.component";
 import { PagesModule } from "./pages/pages.module";
+import { HighligntDirective } from "./commons/highlight.directive";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule, PagesModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, HighligntDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/commons/uppername.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+  selector: "[appUpperName]"
+})
+export class UpperNameDirective {
+  constructor() {}
+}
