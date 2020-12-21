f48826900a4ece31bab6a58f857f1320bd9e5d18
StevenWeng <wosteven@gmail.com>
2020-12-21 23:37:13 +0800

    2 files modified

--- src/app/commons/uppername.directive.ts
+++ src/app/commons/uppername.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, OnInit } from "@angular/core";
+import { Directive, OnInit, Input } from "@angular/core";
 import { HelloComponent } from "../hello.component";
 
 @Directive({
   selector: "[appUpperName]"
 })
 export class UpperNameDirective implements OnInit {
+  @Input() appent: String;
   constructor(private helloComponent: HelloComponent) {}
 
   ngOnInit(): void {
-    this.helloComponent.name = this.helloComponent.name.toUpperCase();
+    this.helloComponent.name =
+      this.helloComponent.name.toUpperCase() + this.appent;
   }
 }
